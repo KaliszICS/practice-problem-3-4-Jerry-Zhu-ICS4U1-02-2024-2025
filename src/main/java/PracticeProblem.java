ff49e11adbd90d88c37c717af71f5f3acc124d83
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-04-08 20:32:20 +0000

    finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,54 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+int n = 10;
+		for(int i = 0; i < n; i++){
+			System.out.println(recaman(10)[i]);
+		}
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+	public static int[] recaman(int n) {
+		
+		if(n <= 0){
+			int[] arr = new int[0];
+			return arr;
+		}
+int[] arr = new int[n];
+for(int i = 0; i < n; i++){
+	arr[i] = recamanHelper(i+1, arr);
 }
+return arr;
+	}
+
+	public static int recamanHelper(int n, int[] arr){
+//base case
+		if (n == 1){
+return 1;
+		}
+boolean isPositive;
+boolean hasValue = false;
+int test = recamanHelper(n-1, arr) - n;
+		if(test > 0){
+isPositive = true;
+		}
+		else{
+		isPositive = false;
+		}
+		for(int i = n-2; i >= 0; i--){
+			if(arr[i] == test){
+				hasValue = true;
+			}
+		}
+
+		if(isPositive && !hasValue){
+			return test;
+		}
+		else{
+			return test + 2*n;
+		}
+		
+	}
+
+	
+	}
+
